0b43289d0911c8ebca1da5ca64315856391fdd7e
Kgaugelo <kgaugelo.ramaramela@umuzi.org>
2019-11-22 13:10:46 +0200

    Project Animal Correction

--- Home.java
+++ Home.java
@@ -3,11 +3,14 @@ package com.company;
 import java.util.ArrayList;
 
 public class Home extends Animal{
+    ArrayList<Animal>petStorage = new ArrayList<Animal>();
 
     void adoptPet(Cat cat){
+        petStorage.add(cat);
         cat.sound();
     }
     void adoptPet(Dog dog){
+        petStorage.add(dog);
         dog.sound();
     }
     void makeAllSounds(){
